64a795aa5f3abee7a7019af814d29d933e64596b
obaldan <taviu522@gmail.com>
2019-01-02 17:01:43 -0200

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import './styles.css'; 
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>test</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+  
+);
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>test</h1>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
