b93edcfb9efe0ffcf70ac39bb0f690e344fa9678
obaldan <taviu522@gmail.com>
2019-01-02 21:39:23 -0200

    styles, pages and api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
+
 import './styles.css'; 
+
+import Header from './components/Header';
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
   
 );
---
+++ src/pages/main/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import './styles.css';
+
+export default class Main extends Component {
+    state = {
+        products: [],
+    };
+
+    componentDidMount() {
+        this.loadProducts();
+    };
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+        this.setState({ products: response.data.docs });
+        console.log(response.data.docs)
+    };
+
+    render(){
+        const { products } = this.state;
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id} className="product-name">
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href={product.url}>Acessar</a>
+                    </article>
+                ))}
+            </div>
+           
+        )
+    }
+};
